329fc23c15a0226881554ed59f415a2df2680936
ismael <yahiani.ismail@gmail.com>
2024-10-28 12:21:39 +0100

    init projetct

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const AppLink = () => {
+  const name="React" ;
+  return (
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+      >
+        Learn {name}
+      </a>
+  )
+}
 function App() {
+  const name="React"
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AppLink></AppLink>
       </header>
     </div>
   );
