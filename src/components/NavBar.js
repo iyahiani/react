015528218aec3ac5f6813b18a48b30b20c40f92f
ismael <yahiani.ismail@gmail.com>
2024-10-30 16:29:47 +0100

    formulaire

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -53,8 +53,60 @@ const MyNavBar = () => {
                         </div>
                     </form>
                 </div>
-
+                <button type="button" className="btn btn-primary ms-3" data-bs-toggle="offcanvas"
+                        data-bs-target="#user">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         className="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fillRule="evenodd"
+                              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                    </svg>
+                </button>
             </header>
+            <section className="offcanvas offcanvas-end" id="user">
+                <div className="offcanvas-header">
+                    <h2 className="offcanvas-title">Se connecter</h2>
+                    <button className="btn btn-close" data-bs-dismiss="offcanvas"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <div className="col-12 col-lg-6">
+                        <div className="mb-3 form-floating">
+                            <input type="text" id="name" className="form-control"
+                                   placeholder="Votre Nom"/>
+                            <label htmlFor="name"> Nom</label>
+
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        <div className="mb-3 form-floating">
+                            <input type="text" id="prenom" className="form-control"
+                                   placeholder="Votre Prénom"/>
+                            <label htmlFor="prenom"> Prénom</label>
+
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        <div className="mb-3 form-floating">
+                            <input type="email" id="mail" className="form-control"
+                                   placeholder="Votre Email"/>
+                            <label htmlFor="mail"> Email</label>
+
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        <div className="form-check form-switch">
+                            <input type="checkbox" name="newsletter" value="true" id="newsletter"
+                                   className="form-check-input"/>
+                            <label className="form-check-label" htmlFor="newsletter">
+                                Se souvenir de moi
+                            </label>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        <button className="btn btn-primary pt-2">Se connecter</button>
+                    </div>
+                </div>
+            </section>
         </div>
 
     )
